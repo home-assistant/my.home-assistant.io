d5321908e92e144f11cf46d7ced29e93c1de3454
Paulus Schoutsen <balloob@gmail.com>
2021-02-05 11:10:03 +0000

    Add redirect file

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import typescript from "rollup-plugin-typescript2";
 import commonjs from "rollup-plugin-commonjs";
+import json from "@rollup/plugin-json";
 import nodeResolve from "rollup-plugin-node-resolve";
 import { terser } from "rollup-plugin-terser";
 import entrypointHashmanifest from "rollup-plugin-entrypoint-hashmanifest";
@@ -9,8 +10,9 @@ const production = !process.env.ROLLUP_WATCH;
 const plugins = [
   nodeResolve({}),
   commonjs(),
+  json(),
   typescript(),
-  production && terser()
+  production && terser(),
 ];
 
 export default [
@@ -20,11 +22,11 @@ export default [
       dir: "dist/",
       format: "es",
       entryFileNames: production ? "[name]-[hash].js" : "[name].js",
-      chunkFileNames: "[name]-[hash].js"
+      chunkFileNames: "[name]-[hash].js",
     },
     plugins: [
       ...plugins,
-      production && entrypointHashmanifest({ manifestName: "manifest.json" })
-    ]
+      production && entrypointHashmanifest({ manifestName: "manifest.json" }),
+    ],
   },
 ];
